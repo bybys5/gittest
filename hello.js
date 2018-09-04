e87b8ec9c3cb9d22b88653b17dc9222e7f928e86
bryce <52269797@qq.com>
2018-09-04 10:58:03 +0800

    about node

---
+++ hello.js
@@ -0,0 +1,5 @@
+'use strict';
+let hello = function(name){
+	return `hello, ${name}`;
+}
+module.exports = hello;
---
+++ main.js
@@ -0,0 +1,3 @@
+'use strict';
+var hello = require('./hello')
+console.log(hello("bryce"))
